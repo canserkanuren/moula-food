5a345285ca8a8f6b4cf3a7919e029c2951795d81
canserkanuren <canserkan.uren@gmail.com>
2019-12-20 15:47:58 +0100

    :bug: remove prop types

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { Text } from 'react-native';
 
 export default class SearchScreen extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
-
   render() {
     return (
       <Text>SearchScreen</Text>
