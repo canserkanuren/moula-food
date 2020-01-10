4b514e3a95a57e6737d0151976de37f4bf7733b7
Can Serkan UREN <canserkan.uren@gmail.com>
2020-01-10 14:57:05 +0100

    Scanner (#2)
    * :sparkles: init scanner page
    * :sparkles: scanner screen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import ScannerButton from '../components/common/ScannerButton';
 
 export default class SearchScreen extends Component {
   render() {
     return (
-      <Text>SearchScreen</Text>
+      <View style={{ flex: 1 }}>
+        <ScannerButton navigation={this.props.navigation}>
+          <Text>Bonjour</Text>          
+        </ScannerButton>
+      </View>
     )
   }
 }
+
+
+// import React, {Component} from 'react';
+// import {Share, Button} from 'react-native';
+
+// class ShareExample extends Component {
+//   onShare = async () => {
+//     try {
+//       const result = await Share.share({
+//         message:
+//           'React Native | A framework for building native apps using React',
+//       });
+
+//       if (result.action === Share.sharedAction) {
+//         if (result.activityType) {
+//           // shared with activity type of result.activityType
+//         } else {
+//           // shared
+//         }
+//       } else if (result.action === Share.dismissedAction) {
+//         // dismissed
+//       }
+//     } catch (error) {
+//       alert(error.message);
+//     }
+//   };
+
+//   render() {
+//     return <Button onPress={this.onShare} title="Share" />;
+//   }
+// }
