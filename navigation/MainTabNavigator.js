0186588489dd0456c769ccd5b8c9de151924a1a3
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2020-01-10 14:55:13 +0100

    Création Page 'Historique' - Statique

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -3,10 +3,25 @@ import Icon from "react-native-vector-icons/Ionicons";
 
 import { createAppContainer } from "react-navigation";
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { createStackNavigator } from "react-navigation-stack";
 
 import SearchScreen from '../screens/SearchScreen';
 import HistoryScreen from '../screens/HistoryScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
+
+const historyNavigator = createStackNavigator({
+  History: { screen: HistoryScreen }
+}, {
+  initialRouteName: 'History',
+  defaultNavigationOptions: {
+      headerStyle: {
+          backgroundColor: '#EEEEEE'
+      },
+      headerTitleStyle: {
+          fontWeight: 'bold'
+      }
+  }
+});
 
 const tabNavigator = createMaterialBottomTabNavigator(
   {
@@ -21,11 +36,11 @@ const tabNavigator = createMaterialBottomTabNavigator(
       }
     },
     History: {
-      screen: HistoryScreen,
+      screen: historyNavigator,
       navigationOptions: {
-        tabBarLabel: 'Paramètres',
+        tabBarLabel: 'Historique',
         tabBarIcon: ({ tintColor }) => (
-          <Icon color={tintColor} size={25} name={'ios-settings'} />
+          <Icon color={tintColor} size={25} name={'ios-clock'} />
         ),
         barStyle: { backgroundColor: 'teal' }
       }
--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,10 +1,144 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native';
+import { SafeAreaView, ScrollView, View, Image, Text } from 'react-native';
+
+import historyStyle from '../assets/styles/historyStyle.js';
 
 export default class HistoryScreen extends Component {
+
+  state = {
+    historyItems: [
+      {
+        barcode: '3256223510025',
+        name: 'Eau de source de montagne'
+      },
+      {
+        barcode: '3256223510025',
+        name: 'Eau de source de montagne'
+      },
+      {
+        barcode: '3256223510025',
+        name: 'Eau de source de montagne'
+      },
+      {
+        barcode: '3256223510025',
+        name: 'Eau de source de montagne'
+      }
+    ]
+  }
+
+  static navigationOptions = (e) => {
+    return {
+      title: 'Historique'
+    }
+  }
+
   render() {
     return (
-      <Text>HistoryScreen</Text>
+      <SafeAreaView style={historyStyle.container}>
+        <ScrollView style={historyStyle.scrollView}>
+          <View style={historyStyle.flexContainer}>
+            <View style={historyStyle.itemCard} >
+              <Image
+                style={historyStyle.itemImage}
+                source={{
+                  uri: 'https://static.openfoodfacts.org/images/products/325/622/351/0032/front_fr.29.full.jpg'
+                }}
+              />
+              <Text style={historyStyle.itemName}> Nom du produit </Text>
+              <Text style={historyStyle.itemBrand}> Marque </Text>
+              <Text style={historyStyle.itemQuality}> Qualité </Text>
+            </View>
+            <View style={historyStyle.itemCard} >
+              <Image
+                style={historyStyle.itemImage}
+                source={{
+                  uri: 'https://static.openfoodfacts.org/images/products/325/622/351/0032/front_fr.29.full.jpg'
+                }}
+              />
+              <Text style={historyStyle.itemName}> Nom du produit </Text>
+              <Text style={historyStyle.itemBrand}> Marque </Text>
+              <Text style={historyStyle.itemQuality}> Qualité </Text>
+            </View>
+            <View style={historyStyle.itemCard} >
+              <Image
+                style={historyStyle.itemImage}
+                source={{
+                  uri: 'https://static.openfoodfacts.org/images/products/325/622/351/0032/front_fr.29.full.jpg'
+                }}
+              />
+              <Text style={historyStyle.itemName}> Nom du produit </Text>
+              <Text style={historyStyle.itemBrand}> Marque </Text>
+              <Text style={historyStyle.itemQuality}> Qualité </Text>
+            </View>
+            <View style={historyStyle.itemCard} >
+              <Image
+                style={historyStyle.itemImage}
+                source={{
+                  uri: 'https://static.openfoodfacts.org/images/products/325/622/351/0032/front_fr.29.full.jpg'
+                }}
+              />
+              <Text style={historyStyle.itemName}> Nom du produit </Text>
+              <Text style={historyStyle.itemBrand}> Marque </Text>
+              <Text style={historyStyle.itemQuality}> Qualité </Text>
+            </View>
+            <View style={historyStyle.itemCard} >
+              <Image
+                style={historyStyle.itemImage}
+                source={{
+                  uri: 'https://static.openfoodfacts.org/images/products/325/622/351/0032/front_fr.29.full.jpg'
+                }}
+              />
+              <Text style={historyStyle.itemName}> Nom du produit </Text>
+              <Text style={historyStyle.itemBrand}> Marque </Text>
+              <Text style={historyStyle.itemQuality}> Qualité </Text>
+            </View>
+            <View style={historyStyle.itemCard} >
+              <Image
+                style={historyStyle.itemImage}
+                source={{
+                  uri: 'https://static.openfoodfacts.org/images/products/325/622/351/0032/front_fr.29.full.jpg'
+                }}
+              />
+              <Text style={historyStyle.itemName}> Nom du produit </Text>
+              <Text style={historyStyle.itemBrand}> Marque </Text>
+              <Text style={historyStyle.itemQuality}> Qualité </Text>
+            </View>
+            <View style={historyStyle.itemCard} >
+              <Image
+                style={historyStyle.itemImage}
+                source={{
+                  uri: 'https://static.openfoodfacts.org/images/products/325/622/351/0032/front_fr.29.full.jpg'
+                }}
+              />
+              <Text style={historyStyle.itemName}> Nom du produit </Text>
+              <Text style={historyStyle.itemBrand}> Marque </Text>
+              <Text style={historyStyle.itemQuality}> Qualité </Text>
+            </View>
+            <View style={historyStyle.itemCard} >
+              <Image
+                style={historyStyle.itemImage}
+                source={{
+                  uri: 'https://static.openfoodfacts.org/images/products/325/622/351/0032/front_fr.29.full.jpg'
+                }}
+              />
+              <Text style={historyStyle.itemName}> Nom du produit </Text>
+              <Text style={historyStyle.itemBrand}> Marque </Text>
+              <Text style={historyStyle.itemQuality}> Qualité </Text>
+            </View>
+            <View style={historyStyle.itemCard} >
+              <Image
+                style={historyStyle.itemImage}
+                source={{
+                  uri: 'https://static.openfoodfacts.org/images/products/325/622/351/0032/front_fr.29.full.jpg'
+                }}
+              />
+              <Text style={historyStyle.itemName}> Nom du produit </Text>
+              <Text style={historyStyle.itemBrand}> Marque </Text>
+              <Text style={historyStyle.itemQuality}> Qualité </Text>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     )
   }
 }
