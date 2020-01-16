18d650871d1af2b14dbdbae073c8e9faaf3e9531
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2020-01-16 12:28:51 +0100

    Setup Redux 'Liste de courses'

--- redux/store.js
+++ redux/store.js
@@ -3,10 +3,14 @@ import thunk from 'redux-thunk';
 
 import servicesReducer from './reducers/servicesReducer';
 import productsReducer from './reducers/productsReducer';
+import shoppingReducer from './reducers/shoppingReducer';
 
 const reducers = combineReducers({
-    services: servicesReducer,
-    products: productsReducer
+  services: servicesReducer,
+  products: combineReducers({
+    general: productsReducer,
+    shopping: shoppingReducer
+  })
 });
 
 export const store = createStore(reducers, applyMiddleware(thunk));
--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class FavoritesScreen extends Component {
+import { initShoppingList, clearShoppingList, delFromShoppingList } from '../redux/actions/shoppingActions';
+
+class FavoritesScreen extends Component {
+
+  componentDidMount() {
+    this.props.shopping.init();
+  }
+
   render() {
     return (
       <Text>FavoritesScreen</Text>
     )
   }
 }
+
+const mapStateToProps = (stateStore) => {
+  return {
+    shoppingProducts: stateStore.products.shopping.shoppingProducts,
+  }
+}
+
+const mapActionsToProps = (barcode) => {
+  return {
+    shopping: {
+      init: bindActionCreators(initShoppingList, barcode),
+      clear: clearShoppingList(clearShoppingList, barcode),
+      remove: bindActionCreators(delFromShoppingList, barcode)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(FavoritesScreen);
