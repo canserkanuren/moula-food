dfe71c9377476c909238595155aa410d85349b32
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2020-01-16 11:38:05 +0100

    History - Ajout et mise en place du component ProductCard (#3)
    * Mise en place basique de Redux
    
    * Création Page 'Historique' - Statique
    
    * Création et mise en place du component ProductCard

---
+++ assets/styles/historyStyle.js
@@ -0,0 +1,50 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  scrollView: {
+    paddingHorizontal: 20,
+  },
+  flexContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'flex-start'
+  },
+  itemCard: {
+    marginVertical: 7,
+    marginHorizontal: 10,
+    padding: 7,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.20,
+    shadowRadius: 1.41,
+    elevation: 2,
+    maxWidth: Math.round((Dimensions.get('window').width) / 2)
+  },
+  itemImage: {
+    width: 150,
+    height: 150, 
+    resizeMode:'contain'
+  },
+  itemName: {
+    fontSize: 16,
+    fontWeight: '500',
+    marginVertical: 4
+  },
+  itemBrand: {
+    color: 'gray'
+  },
+  itemQuality: {
+    color: 'gray'
+  }
+
+});
--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native';
+import { SafeAreaView, ScrollView, View, Image, Text } from 'react-native';
+import ProductCard from '../components/common/ProductCard.js';
+
+import historyStyle from '../assets/styles/historyStyle.js';
 
 export default class HistoryScreen extends Component {
+
+  state = {
+    historyItems: [
+      {
+        barcode: '3256223510025',
+        name: 'Eau de source de montagne',
+        brand: 'Super U',
+        quality: 'Good'
+      },
+      {
+        barcode: '3256223510026',
+        name: 'Eau de source de montagne',
+        brand: 'Super U',
+        quality: 'Good'
+      },
+      {
+        barcode: '3256223510027',
+        name: 'Eau de source de montagne',
+        brand: 'Super U',
+        quality: 'Good'
+      },
+      {
+        barcode: '3256223510028',
+        name: 'Eau de source de montagne',
+        brand: 'Super U',
+        quality: 'Good'
+      }
+    ]
+  }
 
   static navigationOptions = (e) => {
     return {
@@ -11,7 +43,17 @@ export default class HistoryScreen extends Component {
 
   render() {
     return (
-      <Text>HistoryScreen</Text>
+      <SafeAreaView style={historyStyle.container}>
+        <ScrollView style={historyStyle.scrollView}>
+          <View style={historyStyle.flexContainer}>
+            {
+              this.state.historyItems.map((item, key) =>
+                <ProductCard product={item} key={item.barcode}/>
+              )
+            }
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     )
   }
 }
