5960af7bbf6e0f593f2804c6230ddc9cd70c79eb
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2020-01-16 16:40:24 +0100

    Redux (#5)
    * Mise en place basique de Redux
    
    * Setup Redux 'Liste de courses'
    
    * Setup Redux 'Liste de courses'

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,38 +1,18 @@
 import React, { Component } from 'react'
 import { SafeAreaView, ScrollView, View, Image, Text } from 'react-native';
 import ProductCard from '../components/common/ProductCard.js';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-import historyStyle from '../assets/styles/historyStyle.js';
+import { initHistorySearchList, clearHistorySearchList, delFromHistorySearchList } from '../redux/actions/historyActions';
+import { initHistoryScanList, clearHistoryScanList, delFromHistoryScanList } from '../redux/actions/historyActions';
 
-export default class HistoryScreen extends Component {
+import historyStyle from '../assets/styles/historyStyle';
+
+class HistoryScreen extends Component {
 
   state = {
-    historyItems: [
-      {
-        barcode: '3256223510025',
-        name: 'Eau de source de montagne',
-        brand: 'Super U',
-        quality: 'Good'
-      },
-      {
-        barcode: '3256223510026',
-        name: 'Eau de source de montagne',
-        brand: 'Super U',
-        quality: 'Good'
-      },
-      {
-        barcode: '3256223510027',
-        name: 'Eau de source de montagne',
-        brand: 'Super U',
-        quality: 'Good'
-      },
-      {
-        barcode: '3256223510028',
-        name: 'Eau de source de montagne',
-        brand: 'Super U',
-        quality: 'Good'
-      }
-    ]
+    historyProducts: null
   }
 
   static navigationOptions = (e) => {
@@ -41,19 +21,66 @@ export default class HistoryScreen extends Component {
     }
   }
 
+  componentDidMount() {
+    this.props.history.scanned.init();
+    this.setState({ historyProducts: this.props.products.scanned.concat(this.props.products.searched) });
+  }
+
   render() {
     return (
       <SafeAreaView style={historyStyle.container}>
-        <ScrollView style={historyStyle.scrollView}>
-          <View style={historyStyle.flexContainer}>
+        {this.state.historyProducts ? (
+          <View>
             {
-              this.state.historyItems.map((item, key) =>
-                <ProductCard product={item} key={item.barcode}/>
-              )
-            }
+              this.state.historyProducts.length > 0 ? (
+                <ScrollView style={historyStyle.scrollView}>
+                  <View style={historyStyle.flexContainer}>
+                    {
+                      historyProducts.map((item, key) =>
+                        <ProductCard product={item} key={item.barcode} />
+                      )
+                    }
+                  </View>
+                </ScrollView>
+              ) : (
+                  <Text>Aucun historique disponible</Text>
+                )}
           </View>
-        </ScrollView>
-      </SafeAreaView>
+        ) : (
+            // TODO : A Remplacer par un spinner
+            <Text style={historyStyle.flexContainer}>
+              Spinner
+              </Text>
+          )}
+      </SafeAreaView >
     )
   }
 }
+
+const mapStateToProps = (stateStore) => {
+  return {
+    products: {
+      searched: stateStore.products.history.searched,
+      scanned: stateStore.products.history.scanned,
+    }
+  }
+}
+
+const mapActionsToProps = (barcode) => {
+  return {
+    history: {
+      searched: {
+        init: bindActionCreators(initHistorySearchList, barcode),
+        clear: bindActionCreators(clearHistorySearchList, barcode),
+        remove: bindActionCreators(delFromHistorySearchList, barcode)
+      },
+      scanned: {
+        init: bindActionCreators(initHistoryScanList, barcode),
+        clear: bindActionCreators(clearHistoryScanList, barcode),
+        remove: bindActionCreators(delFromHistoryScanList, barcode)
+      }
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(HistoryScreen);
---
+++ services/OpenFFService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const apiKey = ``;
+
+const apiUrl = ``;
+
+class OpenFFService {
+
+}
+
+export default OpenFFService;
